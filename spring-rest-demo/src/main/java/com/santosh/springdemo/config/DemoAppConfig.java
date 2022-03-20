666c1bea81436cfefdae3571166f4bc7a4dd4740
Mutyala Santosh <santosh.mutyala@zemosolabs.com>
2022-03-20 20:37:04 +0530

    Creating rest controller

---
+++ spring-rest-demo/src/main/java/com/santosh/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.santosh.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.santosh.springdemo")
+public class DemoAppConfig {
+
+}
